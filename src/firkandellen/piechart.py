400e9a0ba8e3de5b8ddcee05a607967014651d5b
viktor40 <vannieuwenhuize.viktor@gmail.com>
2021-09-12 22:30:47 +0200

    added tests

---
+++ src/firkandellen/piechart.py
@@ -0,0 +1,9 @@
+import plotly.express as px
+
+
+def pie_chart():
+    pass
+
+
+if __name__ == '__main__':
+    pie_chart()
---
+++ tests/tests_frikandellen.py
@@ -0,0 +1,7 @@
+from src.firkandellen.barchart import bar_chart
+from src.firkandellen.piechart import pie_chart
+
+
+def test_plots():
+    bar_chart()
+    pie_chart()
